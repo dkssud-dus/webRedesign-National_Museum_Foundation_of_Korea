3479b865d1c1b5439b48a4bfbbd5be646a49281b
dkssud-dus <chae3929@gmail.com>
2025-07-16 17:34:36 +0900

    Update Content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
 /* ========================
   Import
 ======================== */
-import './App.css';
+import React, { useState } from 'react';
+
+// component
+import Header from './components/common/Header/Header';
+import Footer from './components/common/Footer';
+import LoginModal from './components/common/LoginModal/LoginModal';
+import SitemapModal from './components/common/SitemapModal/SitemapModal';
+
+// pages
+import Home from './pages/Home';
+import Directions from './pages/Directions';
+import ExhibitionPerformance from './pages/ExhibitionPerformance';
+import Schedule from './pages/Schedule';
+import Events from './pages/Events';
+import Notices from './pages/Notices';
+import NotFoundPage from './pages/NotFoundPage ';
+
+// react-router-dom
+import { Routes, Route } from 'react-router-dom'
 
 // SCSS
 import './styles/style.scss';
 
+/* ========================
+
+======================== */
 function App() {
+  // Modal control
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isSitemapModalOpen, setIsSitemapModalOpen] = useState(false);
+
+  const openLoginModal = () => setIsLoginModalOpen(true);
+  const closeLoginModal = () => setIsLoginModalOpen(false);
+
+  const openSitemapModal = () => setIsSitemapModalOpen(true);
+  const closeSitemapModal = () => setIsSitemapModalOpen(false);
+
+  /* ========================
+  Return
+  ======================== */
   return (
-    <div>
-      응?
+    <div className='App'>
+      <Header 
+        openLoginModal={openLoginModal}
+        openSitemapModal={openSitemapModal}
+      />
+
+      {/* content */}
+      <main className='App__content'>
+        <Routes>
+
+          <Route path='/' element={<Home />}></Route>
+
+          <Route path='/directions' element={<Directions />}></Route>
+
+          <Route path='/exhibitionPerformance' element={<ExhibitionPerformance />}></Route>
+
+          <Route path='/schedule' element={<Schedule />}></Route>
+
+          <Route path='/events' element={<Events />}></Route>
+
+          <Route path='/notices' element={<Notices />}></Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+
+      </main>
+
+      {/* Modal Rendering */}
+      {isLoginModalOpen && <LoginModal onClose={closeLoginModal}/>}
+      {isSitemapModalOpen && <SitemapModal onClose={closeSitemapModal}/>}
+
+      <Footer />
     </div>
   );
 }
